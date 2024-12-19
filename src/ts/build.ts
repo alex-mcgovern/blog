c0a0639be3aa7bcee6a64d5f9c2b1bcbe267f19b
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-12-19 13:37:30 +0000

    chore: format pass

--- src/ts/build.ts
+++ src/ts/build.ts
@@ -1,19 +1,19 @@
-import { blog } from "./builders/blog.js";
-import { home } from "./builders/home.js";
-import { bundleCSS } from "./bundle-css.js";
-import { copyDir, ensureDirExists } from "./fs.js";
-import { registerPartialFile } from "./handlebars.js";
+import { blog } from './builders/blog.js';
+import { home } from './builders/home.js';
+import { bundleCSS } from './bundle-css.js';
+import { copyDir, ensureDirExists } from './fs.js';
+import { registerPartialFile } from './handlebars.js';
 
-ensureDirExists("dist");
-copyDir("src/assets", "dist/assets");
-await bundleCSS("src/css/index.css", "dist/css/index.css");
+ensureDirExists('dist');
+copyDir('src/assets', 'dist/assets');
+await bundleCSS('src/css/index.css', 'dist/css/index.css');
 
-registerPartialFile("head", "src/partials/head.hbs");
-registerPartialFile("nav", "src/partials/nav.hbs");
-registerPartialFile("footer", "src/partials/footer.hbs");
-registerPartialFile("posts", "src/partials/posts.hbs");
-registerPartialFile("postHeader", "src/partials/post-header.hbs");
-registerPartialFile("workExperience", "src/partials/work-experience.hbs");
+registerPartialFile('head', 'src/partials/head.hbs');
+registerPartialFile('nav', 'src/partials/nav.hbs');
+registerPartialFile('footer', 'src/partials/footer.hbs');
+registerPartialFile('posts', 'src/partials/posts.hbs');
+registerPartialFile('postHeader', 'src/partials/post-header.hbs');
+registerPartialFile('workExperience', 'src/partials/work-experience.hbs');
 
 await home();
 await blog();
--- src/ts/bundle-css.ts
+++ src/ts/bundle-css.ts
@@ -1,46 +1,46 @@
-import * as fs from "fs";
-import * as path from "path";
-import { ensureDirExists } from "./fs.js";
+import * as fs from 'fs';
+import * as path from 'path';
+import { ensureDirExists } from './fs.js';
 
 async function processImports(
-  content: string,
-  baseDir: string
+    content: string,
+    baseDir: string,
 ): Promise<string> {
-  const importRegex = /@import\s+['"]([^'"]+)['"];/g;
-  let match;
-  // biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
-  while ((match = importRegex.exec(content)) !== null) {
-    const importPath = match[1];
-    if (!importPath) {
-      throw new Error("Invalid import statement");
+    const importRegex = /@import\s+['"]([^'"]+)['"];/g;
+    let match;
+    // biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+    while ((match = importRegex.exec(content)) !== null) {
+        const importPath = match[1];
+        if (!importPath) {
+            throw new Error('Invalid import statement');
+        }
+        const fullImportPath = path.resolve(baseDir, importPath);
+        const importContent = fs.readFileSync(fullImportPath, 'utf-8');
+        const processedImportContent = await processImports(
+            importContent,
+            path.dirname(fullImportPath),
+        );
+        content = content.replace(match[0], processedImportContent);
     }
-    const fullImportPath = path.resolve(baseDir, importPath);
-    const importContent = fs.readFileSync(fullImportPath, "utf-8");
-    const processedImportContent = await processImports(
-      importContent,
-      path.dirname(fullImportPath)
-    );
-    content = content.replace(match[0], processedImportContent);
-  }
-  return content;
+    return content;
 }
 
 export async function bundleCSS(entryPoint: string, outputPath: string) {
-  // Validate that the input file is a CSS file
-  if (path.extname(entryPoint) !== ".css") {
-    throw new Error("The input file must be a CSS file.");
-  }
+    // Validate that the input file is a CSS file
+    if (path.extname(entryPoint) !== '.css') {
+        throw new Error('The input file must be a CSS file.');
+    }
 
-  // Read the content of the input file
-  let cssContent = fs.readFileSync(entryPoint, "utf-8");
+    // Read the content of the input file
+    let cssContent = fs.readFileSync(entryPoint, 'utf-8');
 
-  // Function to process @import statements
+    // Function to process @import statements
 
-  // Process the @import statements in the CSS content
-  cssContent = await processImports(cssContent, path.dirname(entryPoint));
+    // Process the @import statements in the CSS content
+    cssContent = await processImports(cssContent, path.dirname(entryPoint));
 
-  ensureDirExists(outputPath.split("/").slice(0, -1).join("/"));
+    ensureDirExists(outputPath.split('/').slice(0, -1).join('/'));
 
-  // Write the processed content to the output file
-  fs.writeFileSync(outputPath, cssContent, "utf-8");
+    // Write the processed content to the output file
+    fs.writeFileSync(outputPath, cssContent, 'utf-8');
 }
